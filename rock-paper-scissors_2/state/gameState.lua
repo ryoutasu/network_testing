aa101ff6c505bc4a41d8f85133b752c4ae2ec4d1
Padparadscha <dr_maad@live.com>
2024-07-12 00:25:03 +1000

    added signs

--- rock-paper-scissors_2/state/gameState.lua
+++ rock-paper-scissors_2/state/gameState.lua
@@ -1,3 +1,5 @@
+local tween = require 'lib.tween'
+
 local GameState = Class{}
 
 local center_x = WIN_WIDTH / 2
@@ -10,6 +12,44 @@ local button_h = 150
 
 local button_y = WIN_HEIGHT - button_h - button_offset
 local button_y_clicked = button_y - 15
+
+local line_y = center_y
+local lineLength = 600
+local lineColorGreen = { 0, 1, 0, 1 }
+local lineColorRed = { 1, 0, 0, 1 }
+local roundTime = 10
+local lineExtensionTime = 1
+
+function GameState:lock()
+    self.rockButton:disable()
+    self.paperButton:disable()
+    self.scissorsButton:disable()
+end
+
+function GameState:setSign(sign)
+    self:lock()
+    self.rockButton.y = button_y
+    self.paperButton.y = button_y
+    self.scissorsButton.y = button_y
+
+    local state = 'ready'
+    local opponent = self.opponent
+    if self.currentSign == sign then
+        self.currentSign = nil
+        state = 'unready'
+    else
+        self.currentSign = sign
+        self[sign..'Button'].y = button_y_clicked
+
+        if opponent and opponent.ready then
+            -- begin countdown
+        end
+    end
+
+    if opponent then
+        Host:send(state, opponent.ip, opponent.port)
+    end
+end
 
 function GameState:init()
     local u = Urutora:new()
@@ -53,19 +93,13 @@ function GameState:init()
     }):disable()
 
     rockButton:action(function (e)
-        e.target.y = button_y_clicked
-        paperButton.y = button_y
-        scissorsButton.y = button_y
+        self:setSign('rock')
     end)
     paperButton:action(function (e)
-        e.target.y = button_y_clicked
-        rockButton.y = button_y
-        scissorsButton.y = button_y
+        self:setSign('paper')
     end)
     scissorsButton:action(function (e)
-        e.target.y = button_y_clicked
-        rockButton.y = button_y
-        paperButton.y = button_y
+        self:setSign('scissors')
     end)
     -- Game controls
 
@@ -83,9 +117,19 @@ function GameState:init()
 
     self.u = u
     self.opponent = false
+    self.currentSign = nil
+
+    self.line = {
+        x = center_x, y = line_y,
+        length = 0,
+        color = { 1, 1, 1, 1 },
+        extensioning = false,
+    }
+    self.tweenLineColor = nil
+    self.tweenLineLength = nil
 end
 
-function GameState:enter_host()
+function GameState:enterHost()
     local w, h = 400, 40
     local x, y = center_x - w/2, center_y - h
     local waitOpponentLabel = Urutora.label({
@@ -101,9 +145,9 @@ function GameState:enter_host()
     Host:broadcast({ cmd = 'server_up', name = Player.name })
 end
 
-function GameState:set_opponent(name, ip, port)
+function GameState:setOpponent(name, ip, port)
     self.opponentLabel.text = name
-    self.opponent = { name = name, ip = ip, port = port }
+    self.opponent = { name = name, ip = ip, port = port, ready = false }
 
     if self.is_host then
         Host:broadcast('remove_from_list')
@@ -113,20 +157,24 @@ function GameState:set_opponent(name, ip, port)
     self.rockButton:enable()
     self.paperButton:enable()
     self.scissorsButton:enable()
+
+    self:startRound()
 end
 
 function GameState:enter(state, server, name)
     self.is_host = (server.ip == Host.ip and server.port == Host.port)
 
     if self.is_host then
-        self:enter_host()
+        self:enterHost()
     else
         Host:send({ cmd = 'connect', name = Player.name }, server.ip, server.port)
-        self:set_opponent(name, server.ip, server.port)
+        self:setOpponent(name, server.ip, server.port)
     end
 
     self.playerLabel.text = Player.name
     self.users = 1
+
+    self:startRound()
 end
 
 local timer = 0
@@ -146,24 +194,79 @@ function GameState:receive(dt)
             if data == 'is_server_up' and self.is_host and not self.opponent then
                 Host:send({ cmd = 'server_up', name = Player.name }, msg_or_ip, port_or_nil)
             end
+
+            if data == 'ready' then
+                self.opponent.ready = true
+                if self.sign then
+                    self:lock()
+                end
+            end
         end
 
         if type(data) == "table" then
             if data.cmd == 'connect' then
-                self:set_opponent(data.name, msg_or_ip, port_or_nil)
+                self:setOpponent(data.name, msg_or_ip, port_or_nil)
             end
         end
     end
 end
 
+function GameState:startRound()
+    self.line.length = 0
+    self.line.color = { lineColorRed[1], lineColorRed[2], lineColorRed[3], lineColorRed[4] }
+    self.line.extensioning = true
+    self.tweenLineLength = tween.new(lineExtensionTime, self.line, { length = lineLength })
+    self.tweenLineColor = tween.new(lineExtensionTime, self.line, { color = lineColorGreen })
+end
+
+function GameState:endRound()
+    self.rockButton:disable()
+    self.paperButton:disable()
+    self.scissorsButton:disable()
+    self.rockButton.y = button_y
+    self.paperButton.y = button_y
+    self.scissorsButton.y = button_y
+    self.currentSign = nil
+end
+
+function GameState:updateTween(dt)
+    local tweenLineLength = self.tweenLineLength
+    local tweenLineColor = self.tweenLineColor
+
+    local complete = false
+    if tweenLineLength then
+        complete = tweenLineLength:update(dt)
+    end
+    
+    if tweenLineColor then
+        tweenLineColor:update(dt)
+    end
+
+    if complete then
+        if self.line.extensioning then
+            self.line.extensioning = false
+            self.tweenLineLength = tween.new(roundTime, self.line, { length = 0 }, 'linear')
+            self.tweenLineColor = tween.new(roundTime, self.line, { color = lineColorRed }, 'linear')
+        else
+            self:endRound()
+        end
+    end
+
+    self.line.x = center_x - self.line.length / 2
+end
 
 function GameState:update(dt)
     self:receive(dt)
+    self:updateTween(dt)
     self.u:update(dt)
 end
 
 function GameState:draw()
     self.u:draw()
+
+    local line = self.line
+    love.graphics.setColor(line.color)
+    love.graphics.line(line.x, line.y, line.x + line.length, line.y)
 end
 
 function GameState:mousepressed(x, y, button) self.u:pressed(x, y, button) end
